1324f79134d97e95ddab53d3a4b4f143f6ca1758
parthvyas18896 <pvyas1@confederationcollege.ca>
2020-07-31 21:50:11 -0400

    Did some changes in layout

--- javascript.js
+++ javascript.js
@@ -2,3 +2,4 @@ $(".tab").click(function() {
     $("#" + $(this).attr('id').replace("goto", "")).addClass("active");
     $(this).parent().parent().removeClass("active");
   });
+  <button onclick="survey.html">VISIT PAGE NOW</button>
